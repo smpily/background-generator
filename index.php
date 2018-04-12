9013ed7b058596b62d590f6d25f3043a1d32fda5
[smpily] <37946625+smpily@users.noreply.github.com>
2018-04-12 16:50:48 -0700

    changing title

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
 </head>
 
 <body id="bgcolor">
-    <h1>Background genterator</h1>
+    <h1>Cool Back Groud genterator</h1>
     <h2>current CSS background</h2>
     <input name="color1" class="color1" type="color" value="#00FF00">
     <input name="color2" class="color2" type="color" value="#FF0000">
