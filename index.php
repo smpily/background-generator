0300aebce34ccadcdb287b7b982863157183b593
[smpily] <37946625+smpily@users.noreply.github.com>
2018-04-12 16:31:37 -0700

    Added starting files

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>sitelogan my scripts, your scripts.</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+
+<body id="bgcolor">
+    <h1>Background genterator</h1>
+    <h2>current CSS background</h2>
+    <input name="color1" class="color1" type="color" value="#00FF00">
+    <input name="color2" class="color2" type="color" value="#FF0000">
+
+    <h3></h3>
+    <script type="text/javascript" src="script.js"></script>
+</body>
+
+</html>
