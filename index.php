9c9a7910663e70798a2b6ca477d49ee1ddbc7363
[smpily] <37946625+smpily@users.noreply.github.com>
2018-04-12 17:06:44 -0700

    changed something.

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
 
 <body id="bgcolor">
     <h1>Cool Back Groud genterator</h1>
-    <h2>current CSS background</h2>
+    <h2>cthis is the background</h2>
     <input name="color1" class="color1" type="color" value="#00FF00">
     <input name="color2" class="color2" type="color" value="#FF0000">
 
